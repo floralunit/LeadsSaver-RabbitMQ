b994a4daecdbd3bbe43ff81b247aa309c4dfff12
floralunit <anya71034@gmail.com>
2025-04-19 18:04:52 +0300

    lmp на базу для разработки

--- Consumers/LeadsLMPConsumer.cs
+++ Consumers/LeadsLMPConsumer.cs
@@ -38,7 +38,7 @@ public class LeadsLMPConsumer : IConsumer<RabbitMQLeadMessage_LMP>
     public async Task Consume(ConsumeContext<RabbitMQLeadMessage_LMP> context)
     {
         await Task.Delay(TimeSpan.FromSeconds(2));
-        using var _dbContext = _dbContextFactory.CreateDbContext("ASTRA_AUDI_TEST");
+        using var _dbContext = _dbContextFactory.CreateDbContext("ASCADAII_DEVELOP");
         _logger.LogInformation("NEW LMP MESSAGE Received: LMP Message ({Message}))", context.Message.OuterMessage_ID);
         var entityMessage = await _dbContext.OuterMessage.FirstOrDefaultAsync(e => e.OuterMessage_ID.ToString().ToLower() == context.Message.OuterMessage_ID.ToString().ToLower());
         if (entityMessage == null)
